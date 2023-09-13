863ff1d77d124ec1e9bb74390d2a532598511715
Matthieu MIGNE <matthieu.migne@grenoble-inp.org>
2023-09-13 14:46:28 +0200

    Inital commit

---
+++ main.py
@@ -0,0 +1,87 @@
+# Send list of possible cubes with piece to put in current cube
+def put_piece_in_cube(piece,cube):
+    current_face_index = None
+    for i,f in enumerate(cube):
+        is_empty = True
+        for l in f:
+            if 1 in l:
+                is_empty = False
+                break
+        if is_empty:
+            current_face_index = i
+            break
+
+    # In case of inital face we just put it in the cube
+    if current_face_index == 0:
+        cube[0] = piece
+        return [cube]
+    
+    #In case of putting another face
+    
+
+# DFS back-tracking algorithm with brute force
+def make_cube(pieces_list,cube=None):
+    if cube is None:
+        cube = [[[0] * 5 for _ in range(4)] for _ in range(6)]
+        
+    # Check if cube is complete, if it is the case return True else continue
+    break_check_loop = False
+    for face in cube:
+        for line in face:
+            if 0 in line:
+                break_check_loop = True
+                break
+        if break_check_loop:
+            break
+    # If there is no 0 in the cube, the cube is complete and return True
+    if not break_check_loop:
+        return True
+    
+    # We try for each piece to have every combinations possibles
+    for piece in pieces_list:
+        # List of every cubes possibles for every rotation possibles
+        cube_next_list = put_piece_in_cube(piece,cube)
+        if cube_next_list is None:
+            continue
+
+        for cube_next in cube_next_list:
+            pieces_list_copy = pieces_list.copy()
+            pieces_list_copy.remove(piece)
+            if make_cube(pieces_list_copy, cube_next):
+                return True
+    
+    # If no possible combinations has been found, the cube is not possible for current configuration
+    return False
+
+
+pieces_list = [[[1,1,0,1,1],
+                [1,0,1,0,0],
+                [0,1,0,1,0],
+                [0,0,1,0,1]],
+
+               [[0,0,1,0,1],
+                [1,1,0,1,1],
+                [1,1,0,1,0],
+                [0,0,1,0,0]],
+
+                [[1,1,0,1,0],
+                 [0,1,0,1,0],
+                 [0,1,0,1,0],
+                 [0,0,1,0,1]],
+
+                [[1,0,1,0,0],
+                 [0,1,0,1,1],
+                 [1,0,1,0,0],
+                 [0,1,0,1,1]],
+
+                [[1,1,0,1,0],
+                 [0,0,1,0,0],
+                 [0,0,1,0,0],
+                 [0,0,1,0,1]],
+
+                [[0,1,0,1,0],
+                 [0,0,1,0,0],
+                 [0,1,0,1,0],
+                 [0,0,1,0,0]]]
+
+make_cube(pieces_list)
