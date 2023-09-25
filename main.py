d7f9d1ebc433a3b39a2da2efd4b777f0f9f96583
Matthieu MIGNE <matthieu.migne@orange.fr>
2023-09-25 09:55:51 +0200

    final version

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import copy
 import pprint
+
 import visualizer
+
 pp = pprint.PrettyPrinter(indent=4)
 
 # Set debug to True to have a visualizer displaying the algorithm execution
 debug = False
 
-""" 
+"""
 Assembled cube format:
-              _____________ 
+              _____________
              |    0        |
              |3   <f:1>   3|
              |    2        |
@@ -43,22 +45,22 @@ class Cube:
             target = {2:self.matrix[0][0]}
         elif self.face_to_add == 2:
             target = {0:self.matrix[1][1],
-                       3:self.matrix[0][1]}
+                      3:self.matrix[0][1]}
         elif self.face_to_add == 3:
             target = {1:self.matrix[0][3],
-                       0:self.matrix[1][3]}
+                      0:self.matrix[1][3]}
         elif self.face_to_add == 4:
             target = {0:self.matrix[0][2],
-                       1:self.matrix[2][2],
-                       3:self.matrix[3][2]}
+                      1:self.matrix[2][2],
+                      3:self.matrix[3][2]}
         elif self.face_to_add == 5:
             target = {0:self.matrix[4][2],
-                       1:self.matrix[2][1],
-                       2:self.matrix[1][0],
-                       3:self.matrix[3][3]}     
+                      1:self.matrix[2][1],
+                      2:self.matrix[1][0],
+                      3:self.matrix[3][3]}
 
         #print("face to add:",self.face_to_add)
-        #print("target:",target)       
+        #print("target:",target)
 
         # Check if piece match targeted piece side by rotating it in every directions possibles
         for i in range(8):
@@ -88,7 +90,7 @@ class Cube:
         return False
     
     # Fill current cube with piece
-    def fill(self,piece,face):                 
+    def fill(self,piece,face):
         def slot_pieces(self,face1,side1,face2,side2):
             angle_left = self.matrix[face1][side1][0] | self.matrix[face2][side2][-1]
             angle_right = self.matrix[face1][side1][-1] | self.matrix[face2][side2][0]
@@ -101,27 +103,27 @@ class Cube:
             self.matrix[face2][(side2-1)%4][-1] = angle_right
             self.matrix[face2][(side2+1)%4][0] = angle_left
 
-        self.matrix[face] = copy.deepcopy(piece.matrix)      
+        self.matrix[face] = copy.deepcopy(piece.matrix)
         match face:
             case 1:
-                slot_pieces(self,0,0,1,2)         
+                slot_pieces(self,0,0,1,2)
             case 2:
-                slot_pieces(self,0,1,2,3)   
-                slot_pieces(self,1,1,2,0)    
+                slot_pieces(self,0,1,2,3)
+                slot_pieces(self,1,1,2,0)
             case 3:
-                slot_pieces(self,0,3,3,1)    
-                slot_pieces(self,1,3,3,0)    
-            case 4: 
-                slot_pieces(self,0,2,4,0)   
-                slot_pieces(self,3,2,4,3)   
-                slot_pieces(self,2,2,4,1)  
+                slot_pieces(self,0,3,3,1)
+                slot_pieces(self,1,3,3,0)
+            case 4:
+                slot_pieces(self,0,2,4,0)
+                slot_pieces(self,3,2,4,3)
+                slot_pieces(self,2,2,4,1)
             case 5:
-                slot_pieces(self,5,0,4,2)  
-                slot_pieces(self,5,1,2,1)  
-                slot_pieces(self,5,2,1,0)  
-                slot_pieces(self,5,3,3,3)  
+                slot_pieces(self,5,0,4,2)
+                slot_pieces(self,5,1,2,1)
+                slot_pieces(self,5,2,1,0)
+                slot_pieces(self,5,3,3,3)
                 
-        self.face_to_add = face+1                   
+        self.face_to_add = face+1
 
 class Piece:
     def __init__(self, edge_matrix) -> None:
@@ -137,7 +139,7 @@ class Piece:
 def make_cube(pieces_list, cube=None, visu=None):
     if cube is None:
         # Cube initialisation
-        cube = Cube(pieces_list.pop(0))     
+        cube = Cube(pieces_list.pop(0))
 
     if debug:
         if visu is None:
@@ -155,7 +157,7 @@ def make_cube(pieces_list, cube=None, visu=None):
         pieces_list_copy.pop(i)
         for cube_next in cube_next_list:
             if make_cube(pieces_list_copy, cube_next, visu):
-                return True        
+                return True
     return False
     
 
